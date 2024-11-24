bd0c0bdc822bbd34ee7ca6905c39dfc879370c52
Marc Daniel Gabrillo <marcdanielfoc@marcdaniel.foc.geraldobtd.me>
2024-11-24 04:33:44 +0000

    Initial draft - the code is good - commited by @zavocc
    Same as Lorenz's initial draft from COMMIT SHA:
     edef6507dce13a5a92f774a1e557d984b6d1e8cc
    
    Difference is this also prints odd and even numbers along with all prime numbers
    But needs some optimizations since it prints stack smashing

--- main.c
+++ main.c
@@ -1,10 +1,74 @@
 #include <stdio.h>
 
-// To be filled
-int main(){
-    // Please remove this line
-    printf("Hello World\nI am steve\n");
+int main() 
+{
+    int start, end;
 
-    // Please add relevant code here
+    printf("Enter the starting number: ");
+    scanf("%d", &start);
+    printf("Enter the ending number: ");
+    scanf("%d", &end);
+
+    printf("Prime numbers between %d and %d are:\n", start, end);
+
+    int primes[1000], odd_primes[1000], even_primes[1000];
+    int prime_count = 0, odd_count = 0, even_count = 0;
+
+    for (int i = start; i <= end; i++) 
+    {
+        int is_prime = 1;
+
+        if (i <= 1) 
+        {
+            is_prime = 0;
+        } 
+        else 
+        {
+            for (int j = 2; j * j <= i; j++) 
+            {
+                if (i % j == 0) 
+                {
+                    is_prime = 0;
+                    break;
+                }
+            }
+        }
+
+        
+        if (is_prime) 
+        {
+            primes[prime_count++] = i; 
+            if (i % 2 == 0) 
+            {
+                even_primes[even_count++] = i;  
+            } 
+            else 
+            {
+                odd_primes[odd_count++] = i;
+            }
+        }
+    }
+
+    printf("All prime numbers: ");
+    for (int i = 0; i < prime_count; i++) 
+    {
+        printf("%d ", primes[i]);
+    }
+    printf("\n");
+
+    printf("Odd prime numbers: ");
+    for (int i = 0; i < odd_count; i++) 
+    {
+        printf("%d ", odd_primes[i]);
+    }
+    printf("\n");
+
+    printf("Even prime numbers: ");
+    for (int i = 0; i < even_count; i++) 
+    {
+        printf("%d ", even_primes[i]);
+    }
+    printf("\n");
+
     return 0;
 }
